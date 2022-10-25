ed3922ed2e01b3866e69e66dced25eee1b057bd9
u46cm8 <touch2who@gmail.com>
2022-10-25 20:16:00 +0100

    All have been done until 08 the sum and add functions are the same due to a faulty assumption, the soution took some time so I mark  this point

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,60 @@
 const add = function() {
-	
+//same as sum as this was developed under the assumption that add had to do both
+  let sum = 0;
+
+  for(arg in arguments){
+
+      if(Array.isArray(arguments[arg])){
+
+          for(listed in arguments[arg]){
+
+              sum += arguments[arg][listed];
+          }
+      }
+      else{
+          sum += arguments[arg];
+      }
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
 const sum = function() {
+  //same as sum as this was developed under the assumption that add had to do both
+    let sum = 0;
+  
+    for(arg in arguments){
+  
+        if(Array.isArray(arguments[arg])){
+  
+            for(listed in arguments[arg]){
+  
+                sum += arguments[arg][listed];
+
+            }
+        }
+        else{
+            sum += arguments[arg];
+        }
+    }
+    return sum;
+};
+
+const multiply = function(num1,num2) {
+  return num1 * num2;
+
+};
+
+const power = function(num1,num2) {
+  return num1^num2;
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num1,num2) {
+	return num1%num2;
 };
 
 // Do not edit below this line
